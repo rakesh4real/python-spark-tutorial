e2c93e7ca7756be24bc6610d0d46965b147af456
asapanna rakesh <rakeshark22@gmail.com>
2019-08-03 02:26:24 +0530

    airport soln

--- rdd/airports/AirportsInUsaProblem.py
+++ rdd/airports/AirportsInUsaProblem.py
@@ -1,4 +1,16 @@
-from pyspark import SparkContext
+
+
+import sys
+sys.path.insert(0, '.')
+from pyspark import SparkContext, SparkConf
+from commons.Utils import Utils
+
+
+def splitComma(line: str):
+    splits = Utils.COMMA_DELIMITER.split(line)
+    return "{}, {}".format(splits[1], splits[2])
+
+
 
 if __name__ == "__main__":
 
@@ -15,3 +27,12 @@ if __name__ == "__main__":
     "Dowagiac Municipal Airport", "Dowagiac"
     ...
     '''
+
+    conf = SparkConf().setAppName("airports").setMaster("local[2]")
+    sc = SparkContext(conf = conf)
+
+    airports = sc.textFile("in/airports.text")
+    airportsInUSA = airports.filter(lambda line : Utils.COMMA_DELIMITER.split(line)[3] == "\"United States\"")
+
+    airportsNameAndCityNames = airportsInUSA.map(splitComma)
+    airportsNameAndCityNames.saveAsTextFile("out/airports_in_usa_r.text")
